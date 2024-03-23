a5ffa9135ad12238756f6d24418b11982f235bd9
Milan Wesołowski <milanweso@gmail.com>
2024-03-23 18:24:04 +0100

    Create basic files

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Hoodie Color Picker</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo molestiae illo minima ipsam, eaque nisi totam, aliquam sapiente deleniti, corporis amet temporibus quasi repellat quis. Sed dolorem voluptatum eveniet similique!</p>
+        </header>
+    )
+}
+
+export default Header
