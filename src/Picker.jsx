b6311dd0552eda97cad6430d6da4d08c7841b3d6
Milan Wesołowski <milanweso@gmail.com>
2024-03-25 22:04:46 +0100

    Update Picker

--- src/Picker.jsx
+++ src/Picker.jsx
@@ -1,16 +1,16 @@
+import Randomizer from "./Randomizer"
+
 function Picker() {
     function end() {
         window.location.href = '/header';
-
     }
 
     document.querySelector('body').style.backgroundImage = 'url("BackgroundPicker.jpg")';
-    document.querySelector('body').style.backgroundColor = 'black';
 
     return (
         <div className="picker">
             <img src="1.jpg" alt="" />
-            <button onClick={end}>CHOOSE RANDOM COLOR</button>
+            <button>CHOOSE RANDOM COLOR</button>
             <button onClick={end}>BACK TO MAIN PAGE</button>
         </div>
     )
---
+++ src/Randomizer.jsx
@@ -0,0 +1,5 @@
+function Randomizer() {
+    const balls = 1;
+}
+
+export default Randomizer
