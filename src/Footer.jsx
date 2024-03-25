b926af1d3b5061401d63916c3c03545ee755e608
Milan Wesołowski <milanweso@gmail.com>
2024-03-25 20:36:44 +0100

    Update footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,8 @@
 function Footer() {
     return (
         <footer>
-            <a href="https://www.pexels.com/" target="_blank">IMAGES SOURCED FROM PEXELS</a>
+            <a href="https://www.pexels.com/" target="_blank">BACKGROUNDS SOURCED FROM PEXELS</a>
+            <a href="https://www.kittl.com/" target="_blank">HOODIE IMAGES SOURCED FROM KITTL</a>
         </footer>
     )
 }
