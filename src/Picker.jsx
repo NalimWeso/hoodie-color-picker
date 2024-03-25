7be39cd0adf465bd0218c42c0ae779af3c955e05
Milan Wesołowski <milanweso@gmail.com>
2024-03-25 21:37:52 +0100

    Upgrade Picker

--- src/Picker.jsx
+++ src/Picker.jsx
@@ -4,13 +4,14 @@ function Picker() {
 
     }
 
-    document.querySelector('body').style.backgroundImage = 'url("1.jpg")';
+    document.querySelector('body').style.backgroundImage = 'url("BackgroundPicker.jpg")';
     document.querySelector('body').style.backgroundColor = 'black';
 
     return (
         <div className="picker">
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, velit! Exercitationem ullam officia, illum impedit odio ratione eum, iste dolore eius, sit deserunt. Sint cumque iure quasi quas veritatis vero.</h1 >
-            <button onClick={end}>END</button>
+            <img src="1.jpg" alt="" />
+            <button onClick={end}>CHOOSE RANDOM COLOR</button>
+            <button onClick={end}>BACK TO MAIN PAGE</button>
         </div>
     )
 }
