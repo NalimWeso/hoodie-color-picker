a4a2a218e1ae7bafabac37b384431d163935095c
Milan Wesołowski <milanweso@gmail.com>
2024-03-30 15:02:58 +0100

    Update functions

--- src/Picker.jsx
+++ src/Picker.jsx
@@ -5,9 +5,10 @@ function Picker() {
         window.location.href = '/start';
     }
 
-    document.querySelector('body').style.backgroundImage = 'url("BackgroundPicker.jpg")';
+    document.querySelector('body').style.backgroundImage = 'url("backgrounds/BackgroundPicker.jpg")';
 
     const [number, setNumber] = useState(0);
+    const array = [];
 
     function generateNumber() {
         let newNumber = Math.floor((Math.random() * 7) + 1);
@@ -17,15 +18,28 @@ function Picker() {
         }
 
         setNumber(newNumber);
+
+        // for (let i = 1; i <= 7; i++) {
+        //     // if (number === 0) {
+        //     //     i++;
+        //     // }
+
+        //     if (i !== number) {
+        //         array.push(i);
+        //     }
+
+        // }
+
+        // console.log(array);
     }
 
     const showHoodie = () => {
-        return `${number}.jpg`
+        return <img src={`hoodies/${number}.jpg`} alt="" />
     };
 
     return (
         <div className="picker">
-            <img src={showHoodie()} alt="" />
+            {showHoodie()}
             <button onClick={generateNumber}>CHOOSE RANDOM COLOR</button>
             <button onClick={end}>BACK TO MAIN PAGE</button>
         </div>
