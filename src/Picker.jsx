8788cdc706e6f810b35399ad6e214e860e82d8e1
Milan Wesołowski <milanweso@gmail.com>
2024-03-30 13:57:02 +0100

    Create functions

--- src/Picker.jsx
+++ src/Picker.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 function Picker() {
     function end() {
         window.location.href = '/start';
@@ -5,10 +7,26 @@ function Picker() {
 
     document.querySelector('body').style.backgroundImage = 'url("BackgroundPicker.jpg")';
 
+    const [number, setNumber] = useState(1);
+
+    function generateNumber() {
+        let newNumber = Math.floor((Math.random() * 7) + 1);
+
+        while (newNumber === number) {
+            newNumber = Math.floor((Math.random() * 7) + 1);
+        }
+
+        setNumber(newNumber);
+    }
+
+    const showHoodie = () => {
+        return `${number}.jpg`
+    };
+
     return (
         <div className="picker">
-            <img src="1.jpg" alt="" />
-            <button>CHOOSE RANDOM COLOR</button>
+            <img src={showHoodie()} alt="" />
+            <button onClick={generateNumber}>CHOOSE RANDOM COLOR</button>
             <button onClick={end}>BACK TO MAIN PAGE</button>
         </div>
     )
