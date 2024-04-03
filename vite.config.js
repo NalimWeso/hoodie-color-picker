8ba310f59fd4ec5856e11582f007da7a2d71adb7
Milan Wesołowski <milanweso@gmail.com>
2024-04-03 10:26:30 +0200

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hoodie-color-picker/"
+  base: "https://nalimweso.github.io/hoodie-color-picker/start"
 })
